227ed933636a26f7499a065daaa994f3c624e183
KarenF <karenf@outlook.com>
2020-02-15 19:31:18 -0300

    add initial config

--- src/test/java/com/wallet/HelloWorld.java
+++ src/test/java/com/wallet/HelloWorld.java
@@ -1,18 +1,18 @@
 package com.wallet;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
-class WalletApplicationTests {
+public class HelloWorld {
 
 	@Test
-	void contextLoads() {
+	public void testHelloWorld() {
+		assertEquals(1, 1);
 	}
-
 }
