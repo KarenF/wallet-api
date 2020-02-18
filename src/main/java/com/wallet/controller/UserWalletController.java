908c2c7b232d71196fd895a1a961ebdec8ef21d3
KarenF <karenf@outlook.com>
2020-02-18 14:50:00 -0300

    refactor UserWalletController

--- src/main/java/com/wallet/controller/UserWalletController.java
+++ src/main/java/com/wallet/controller/UserWalletController.java
@@ -19,51 +19,50 @@ import com.wallet.response.Response;
 import com.wallet.service.UserWalletService;
 
 @RestController
-@RequestMapping(name = "user-wallet")
+@RequestMapping(path = "user-wallet")
 public class UserWalletController {
 
 	@Autowired
-	UserWalletService service;
+	private UserWalletService service;
 
 	@PostMapping
 	public ResponseEntity<Response<UserWalletDTO>> create(@Valid @RequestBody UserWalletDTO dto, BindingResult result) {
-		
-		Response<UserWalletDTO> response = new Response<>();
-		
-		if(result.hasErrors()) {
+
+		Response<UserWalletDTO> response = new Response<UserWalletDTO>();
+
+		if (result.hasErrors()) {
 			result.getAllErrors().forEach(r -> response.getErrors().add(r.getDefaultMessage()));
-			
+
 			return ResponseEntity.badRequest().body(response);
 		}
-		
-		UserWallet uw = service.save(this.convertDtoToEntity(dto));
-		
-		response.setData(this.convertEntityToDto(uw));
 
+		UserWallet uw = service.save(this.convertDtoToEntity(dto));
+
+		response.setData(this.convertEntityToDto(uw));
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 
 	public UserWallet convertDtoToEntity(UserWalletDTO dto) {
 		UserWallet uw = new UserWallet();
-		
 		User u = new User();
-		u.setId(dto.getId());
+		u.setId(dto.getUsers());
 		Wallet w = new Wallet();
-		w.setId(dto.getId());
-		
+		w.setId(dto.getWallet());
+
 		uw.setId(dto.getId());
 		uw.setUsers(u);
 		uw.setWallet(w);
-		
+
 		return uw;
 	}
-	
+
 	public UserWalletDTO convertEntityToDto(UserWallet uw) {
 		UserWalletDTO dto = new UserWalletDTO();
 		dto.setId(uw.getId());
 		dto.setUsers(uw.getUsers().getId());
 		dto.setWallet(uw.getWallet().getId());
-		
+
 		return dto;
 	}
+
 }
